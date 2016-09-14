1e3b02a9c0fc84a0f7520c811d3ae2c39c353eb7
yamasita <yamasita@yaasita.net>
2016-09-15 00:29:58 +0900

    update

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func main() {
+	var fp *os.File
+	var err error
+	fp, err = os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(fp)
+	var str []string
+	for scanner.Scan() {
+		str = append(str, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	fp.Close()
+	// 書き出し
+	for _, v := range str {
+		if http := regexp.MustCompile("^http"); http.MatchString(v) {
+			v = http.ReplaceAllString(v, "- http")
+		} else if title := regexp.MustCompile(`^[^\-\#]`); title.MatchString(v) {
+			rep1 := regexp.MustCompile(`[\[\]～－]`)
+			v = rep1.ReplaceAllString(v, "")
+
+			rep2 := regexp.MustCompile(`[\s\,]`)
+			v = rep2.ReplaceAllString(v, "_")
+
+			v = "## " + v
+		}
+		fmt.Println(v)
+	}
+}
